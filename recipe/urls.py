5fd61a7bda7afce1d709c2fe67b5f68756b4d543
Hafiz Fadillah Sandi <hafiz.fadillah@ui.ac.id>
2021-10-23 21:27:56 +0700

    Adding Path to Recipe Detail

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import recipe_page
+from .views import recipe_detail_bruschetta
 urlpatterns = [
     path('', recipe_page),
-
+    path('recipe-bruschetta', recipe_detail_bruschetta)
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def recipe_page(request):
     return render(request, 'recipe_page.html')
+
+def recipe_detail_bruschetta(request):
+    return render(request, 'recipe_detail_bruschetta.html')

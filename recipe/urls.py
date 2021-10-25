a28dbef3159889ae295620f91ec629f0602e29d6
Hafiz Fadillah Sandi <hafiz.fadillah@ui.ac.id>
2021-10-25 23:07:03 +0700

    Updating Navbar and Footer to Recipe Page

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,5 +3,5 @@ from .views import recipe_page
 from .views import recipe_detail_bruschetta
 urlpatterns = [
     path('', recipe_page),
-    path('recipe-bruschetta', recipe_detail_bruschetta)
+    path('bruschetta/', recipe_detail_bruschetta),
 ]

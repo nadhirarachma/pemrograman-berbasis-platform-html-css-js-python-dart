49d85479b423525323f075a0dbccf69259fd5293
Nadhira Rachma Salsabila Anandra <nadhira.anandra08@gmail.com>
2021-10-27 16:04:28 +0700

    Update summary

--- summary/views.py
+++ summary/views.py
@@ -1,25 +1,33 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from .models import Profile
-from .forms import ProfileForm
+from django.contrib.auth.decorators import login_required
+from .forms import UpdateUserForm, UpdateProfileForm
 # from workout.models import Workout
 # from sleep.models import Sleep
 
 def index(request):
-    # profile = Profile.objects.all()
+    profile = request.user.profile
+    full_name = request.user.first_name + " " + request.user.last_name
+    email = request.user.email
+    username = request.user.username
     # workout = Workout.objects.all()
     # sleep = Sleep.objects.all()
 
     # response = {'profile': profile, 'workout' : workout, 'sleep' : sleep}
-    # response = {'profile': profile}
-    return render(request, 'summary.html')
+    response = {'profile': profile, 'full_name': full_name, 'email':email, 'username':username}
+    return render(request, 'summary.html', response)
 
+@login_required
 def edit_profile(request):
-#     form = ProfileForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         return redirect('/summary')
-    
-#     response = {'form' : form}
-    return render(request, 'edit_profile.html')
+    username = request.user.username
+    user_form = UpdateUserForm(request.POST or None, instance=request.user)
+    profile_form = UpdateProfileForm(request.POST or None, instance=request.user.profile)
+
+    if user_form.is_valid() and profile_form.is_valid():
+        user_form.save()
+        profile_form.save()
+        return redirect('/summary')
+
+    response = {'user_form': user_form, 'profile_form': profile_form, 'username':username}
+    return render(request, 'edit_profile.html', response)
+
   

8ce5d137006461b4e35622bd7b0a7b03ec94ae9e
unknown <89336274+huanis@users.noreply.github.com>
2021-10-27 21:37:02 +0700

    update workout

--- workout/views.py
+++ workout/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Exercise
+from .forms import ExerciseForm, TimeForm
+from django.contrib.auth.decorators import login_required
+import datetime
+
+from django.http import HttpResponse
 
 # Create your views here.

e1dc567adcc052ceb5507cce115789a87793e4a1
Nadhira Rachma Salsabila Anandra <nadhira.anandra08@gmail.com>
2021-10-28 21:37:31 +0700

    Update Summary

--- summary/urls.py
+++ summary/urls.py
@@ -3,5 +3,5 @@ from .views import summary, edit_profile
 
 urlpatterns = [
     path('', edit_profile, name = 'edit_profile'),
-    path('activity', summary, name = 'summary'),
+    path('activity/', summary, name = 'summary'),
 ]

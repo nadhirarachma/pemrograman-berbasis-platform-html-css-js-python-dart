d18a0beb87842ad1a2e376097d8304e4b4146a09
Hafiz Fadillah Sandi <hafiz.fadillah@ui.ac.id>
2021-10-29 11:42:46 +0700

    Finalized 1.0

--- recipe/urls.py
+++ recipe/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import recipe_detail_bruschetta , recipe_detail_salmon ,recipe_detail_shrimp, recipe_detail_greek_chicken, recipe_detail_salad,recipe_detail_chicken_soup
 from .views import recipe_page, delete_comment, edit_comment, get_all_comment
 urlpatterns = [
-    path('', recipe_page),
-    path('bruschetta/', recipe_detail_bruschetta),
-    path('honey-walnut-shrimp/', recipe_detail_shrimp),
-    path('salad-caprese-zoodles/', recipe_detail_salad),
-    path('garlicky-greek-chicken/', recipe_detail_greek_chicken),
-    path('baked-salmon/', recipe_detail_salmon),
-    path('chicken-soup/', recipe_detail_chicken_soup),
+    path('', recipe_page, name ='recipe-page'),
+    path('bruschetta/', recipe_detail_bruschetta, name='recipe-detail-bruschetta'),
+    path('honey-walnut-shrimp/', recipe_detail_shrimp, name='recipe-detail-shrimp'),
+    path('salad-caprese-zoodles/', recipe_detail_salad, name='recipe-detail-salad'),
+    path('garlicky-greek-chicken/', recipe_detail_greek_chicken, name='recipe-detail-greek-chicken'),
+    path('baked-salmon/', recipe_detail_salmon, name='recipe-detail-salmon'),
+    path('chicken-soup/', recipe_detail_chicken_soup, name='recipe-detail-chicken-soup'),
     path('delete/<int:id>', delete_comment, name='delete-comment'),
     path('edit/<int:id>', edit_comment, name='edit-comment'),
     path('get_all_comment', get_all_comment, name='get-all-comment'),

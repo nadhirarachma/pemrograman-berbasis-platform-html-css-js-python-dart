69ca496f4d2f89d7da1e6a61db2a8a1368f8444a
nadhira.rachma <nadhira.anandra08@gmail.com>
2021-11-19 17:40:57 +0700

    Lab 7 Checklist Done

---
+++ lab_7/lib/screens/summary.dart
@@ -0,0 +1,400 @@
+import 'package:flutter/material.dart';
+import 'package:lab_7/screens/edit_profile.dart';
+import '../widgets/main_drawer.dart';
+
+class ActivitySummaryApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Activity Summary',
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+        color: const Color(0xFF000000),
+      )),
+      home: ProfileApp(),
+    );
+  }
+}
+
+class ProfileApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("e-Nadi", style: TextStyle(color: Colors.red)),
+        backgroundColor: Colors.black,
+      ),
+      drawer: MainDrawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.black12, Colors.black38]),
+                image: DecorationImage(
+                  image: AssetImage("lib/assets/images/bg4.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    Text(
+                      "WELCOME BACK, NADHIRA!",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "Stay Healthy, Stay Motivated",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      "---------------------",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.yellow[700],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Container(
+                      child: Card(
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 50.0, vertical: 5.0),
+                        clipBehavior: Clip.antiAlias,
+                        color: Color.fromRGBO(242, 248, 243, 0.5),
+                        elevation: 5.0,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 22.0),
+                          child: Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    CircleAvatar(
+                                      backgroundColor: Colors.black,
+                                      radius: 45.0,
+                                    ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    Text(
+                                      "NADHIRA",
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    Text(
+                                      "Female, 19",
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    Text(
+                                      "Student",
+                                      style: TextStyle(
+                                        fontSize: 14.0,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Card(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 50.0, vertical: 5.0),
+                      clipBehavior: Clip.antiAlias,
+                      color: Color.fromRGBO(242, 248, 243, 0.5),
+                      elevation: 5.0,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 22.0),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Column(
+                                children: <Widget>[
+                                  ListTile(
+                                    title: Text(
+                                      "FULL NAME",
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      "Nadhira Rachma",
+                                      style: TextStyle(
+                                        fontSize: 14.0,
+                                        color: Colors.white70,
+                                      ),
+                                    ),
+                                    leading: Icon(Icons.people),
+                                  ),
+                                  ListTile(
+                                    title: Text(
+                                      "EMAIL",
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      "nadhira@gmail.com",
+                                      style: TextStyle(
+                                        fontSize: 14.0,
+                                        color: Colors.white70,
+                                      ),
+                                    ),
+                                    leading: Icon(Icons.email),
+                                  ),
+                                  ListTile(
+                                    title: Text(
+                                      "MOBILE",
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      "085714769898",
+                                      style: TextStyle(
+                                        fontSize: 14.0,
+                                        color: Colors.white70,
+                                      ),
+                                    ),
+                                    leading: Icon(Icons.phone),
+                                  ),
+                                  ListTile(
+                                    title: Text(
+                                      "ADDRESS",
+                                      style: TextStyle(
+                                        fontSize: 16.0,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      "Jakarta, Indonesia",
+                                      style: TextStyle(
+                                        fontSize: 14.0,
+                                        color: Colors.white70,
+                                      ),
+                                    ),
+                                    leading: Icon(Icons.home),
+                                  ),
+                                  SizedBox(
+                                    height: 15.0,
+                                  ),
+                                  TextButton(
+                                    child: Text(
+                                      'EDIT',
+                                      style: TextStyle(
+                                        fontSize: 14.0,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                              Colors.yellow[700]),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  EditProfileApp()));
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    Text(
+                      "ACTIVITY SUMMARY",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Card(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 50.0, vertical: 5.0),
+                      clipBehavior: Clip.antiAlias,
+                      color: Color.fromRGBO(242, 248, 243, 0.5),
+                      elevation: 5.0,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 22.0),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Column(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.fitness_center,
+                                    size: 25,
+                                  ),
+                                  SizedBox(
+                                    height: 8.0,
+                                  ),
+                                  Text(
+                                    "WORKOUT",
+                                    style: TextStyle(
+                                      color: Colors.brown[900],
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    "30 minutes",
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    "Nov. 20, 2021",
+                                    style: TextStyle(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.brown[800],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10.0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.bed,
+                                    size: 25,
+                                  ),
+                                  SizedBox(
+                                    height: 8.0,
+                                  ),
+                                  Text(
+                                    "SLEEP",
+                                    style: TextStyle(
+                                      color: Colors.brown[900],
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    "8 hours",
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                  Text(
+                                    "Nov. 20, 2021",
+                                    style: TextStyle(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.brown[800],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10.0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lab_instruction/lab_7/README.md
+++ lab_instruction/lab_7/README.md
@@ -22,12 +22,12 @@ Anda diminta untuk membuat sebuah app baru di dalam project ini bernama `lab_7` 
 ## Checklist
 
 ### Running Sample Program
-1. [ ] Go to directory `lab_7`in root directory (`pbp-lab`).
-2. [ ] Run `flutter create .` to initiate flutter in your `lab_7` directory. If you would like to try `lab_7` boilerplate, just run `flutter run`.
+1. [x] Go to directory `lab_7`in root directory (`pbp-lab`).
+2. [x] Run `flutter create .` to initiate flutter in your `lab_7` directory. If you would like to try `lab_7` boilerplate, just run `flutter run`.
 
 ### Implement my own Input page
-3. [ ] Try to reuse components and widget from boilerplate and modify it as required. 
-4. [ ] Try the application that you have built in this lab using Web Browser / Mobile Devices / Simulator, just run `flutter run`.
+3. [x] Try to reuse components and widget from boilerplate and modify it as required. 
+4. [x] Try the application that you have built in this lab using Web Browser / Mobile Devices / Simulator, just run `flutter run`.
 
 ## Referensi
 

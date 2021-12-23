3c9fb40ee4b2ec23ea236c805c3bc2e97d52314f
arsyaninsa19 <91448388+arsyaninsa19@users.noreply.github.com>
2021-12-23 13:13:44 +0700

    JSOn update HOME

--- home/urls.py
+++ home/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from django.conf.urls import url
 
-from .views import index, news_page, feedback
+from .views import index, news_page, feedback, get_all_feedback, get_all_news
 
 
 
 urlpatterns = [
     path('', index, name='index'), 
     path('feedback', feedback, name='feedback' ),
+    path('getfeedback', get_all_feedback, name='get-all-feedback'),
+    path('getnews', get_all_news, name='get-all-news'),
     path('<str:slug>/', news_page, name='readmore'),
+    
     
     
 ]
--- home/views.py
+++ home/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import FeedBack, News
+
 from .forms import FeedBackForm
 from django.http import HttpResponse, HttpResponseRedirect, response
 from django.contrib.auth.forms import UserModel
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
+from django.http.response import HttpResponseRedirect,  HttpResponse, JsonResponse
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
+from .models import FeedBack, News
 
 # Create your views here.
 @login_required(login_url="/admin/login/")
@@ -16,6 +18,17 @@ def feedback(request):
     response = {'news_list': news_list, 'feedback_list': feedback_list}
     return render(request, 'feedback.html', response )
 
+@csrf_exempt
+def get_all_news(request):
+    all_news = FeedBack.objects.all()
+    data = serializers.serialize('json', all_news)
+    return HttpResponse(data, content_type="application/json")
+
+@csrf_exempt
+def get_all_feedback(request):
+    all_feedback = News.objects.all()
+    data = serializers.serialize('json', all_feedback)
+    return HttpResponse(data, content_type="application/json")
 
 def index(request):
     news_list = News.objects.all()
@@ -48,14 +61,3 @@ def news_page(request, slug):
     response = {'new':  new}
     return render(request, 'home_news.html', response)
 
-@csrf_exempt
-def get_all_news(request):
-    all_news = FeedBack.objects.all()
-    data = serializers.serialize('json', all_news)
-    return HttpResponse(data, content_type="application/json")
-
-@csrf_exempt
-def get_all_feedback(request):
-    all_feedback = News.objects.all()
-    data = serializers.serialize('json', all_feedback)
-    return HttpResponse(data, content_type="application/json")
